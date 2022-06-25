6d39a4eb530126c87f409e4a279b44d4d0342d65
zormax <zormax052@gmail.com>
2022-06-25 05:56:50 +0300

    end of lesson 92

--- client/src/app/shop/shop.module.ts
+++ client/src/app/shop/shop.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ShopComponent } from './shop.component';
 import { SharedModule } from '../shared/shared.module';
+import { ProductItemComponent } from './product-item/product-item.component';
 
 
 
 @NgModule({
-  declarations: [ShopComponent],
+  declarations: [ShopComponent, ProductItemComponent],
   imports: [
     CommonModule,
     SharedModule
